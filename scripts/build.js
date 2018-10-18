4360da755f0648c69c6e05f08397ca8530518f6d
fisker <lionkay@gmail.com>
2018-10-18 11:52:22 +0800

    update

--- scripts/build.js
+++ scripts/build.js
@@ -3,12 +3,14 @@ const CHARSET = 'utf-8'
 const fs = require('fs')
 const nodeSass = require('node-sass')
 const octicons = require('octicons')
-const babel = require('babel-core')
+const babel = require('@babel/core')
 const prettier = require('prettier')
 const stringify = require('json-stable-stringify')
 const babelConfig = JSON.parse(fs.readFileSync('../.babelrc', CHARSET))
 
-const btoa = global.btoa || require('btoa')
+const SVGO = require('svgo')
+const svgToMiniDataURI = require('mini-svg-data-uri')
+const svgo = new SVGO()
 
 const baseStyle = nodeSass
   .renderSync({
@@ -36,29 +38,52 @@ const asserts = {
   }
 }
 
-function getIcon(name) {
-  const svg = octicons[name].toSVG({
-    fill: '#6a737d',
-    class: '',
-    xmlns: 'http://www.w3.org/2000/svg'
-  }).replace(' class=""', '')
-  const img = 'data:image/svg+xml;base64,' + btoa(svg)
-  // ie can't recognize
-  // const img = 'data:image/svg+xml;utf8,' + encodeURIComponent(svg)
-  return img
+function getIcon(icon) {
+  return svgo.optimize(icon.content).then(function(result) {
+    let uri = svgToMiniDataURI(result.data)
+    uri = uri.replace('data:image/svg+xml,', 'data:image/svg+xml;charset=utf-8,')
+    return {
+      ...icon,
+      uri
+    }
+  })
 }
 
-Object.keys(asserts.icons).forEach(function(type) {
-  asserts.icons[type] = getIcon(asserts.icons[type])
-})
+function getIcons() {
+  const icons = Object.keys(asserts.icons).map(name => {
+    let octiconName = asserts.icons[name]
+    let content = octicons[octiconName].toSVG({
+      fill: '#6a737d',
+      class: '',
+      'aria-hidden': '',
+      xmlns: 'http://www.w3.org/2000/svg'
+    })
 
-fs.writeFileSync('../dist/asserts.json', stringify(asserts, {space: 2}))
-fs.writeFileSync('../dist/index.js', (function() {
-  let code = fs.readFileSync('../src/index.js', CHARSET)
-  code = babel.transform(code, babelConfig).code
-  code = prettier.format(
-    code,
-    prettier.resolveConfig.sync('prettier.config.js')
+    return {
+      name,
+      content
+    }
+  })
+
+  return Promise.all(icons.map(getIcon)).then(icons => {
+    icons.forEach(icon => {
+      asserts.icons[icon.name] = icon.uri
+    })
+  })
+}
+
+getIcons().then(() => {
+  fs.writeFileSync('../lib/asserts.json', stringify(asserts, {space: 2}))
+  fs.writeFileSync(
+    '../lib/index.js',
+    (() => {
+      let code = fs.readFileSync('../src/index.js', CHARSET)
+      code = babel.transform(code, babelConfig).code
+      code = prettier.format(
+        code,
+        prettier.resolveConfig.sync('../lib/index.js')
+      )
+      return code
+    })()
   )
-  return code
-})())
+})

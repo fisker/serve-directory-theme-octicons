d67d616371bbfa35a0c08fd21ca2b782073ba8de
fisker <lionkay@gmail.com>
2019-06-25 19:40:54 +0800

    chore: update config

--- src/index.js
+++ src/index.js
@@ -33,9 +33,7 @@ function getIconName(file) {
 }
 
 function iconToCSS(icon) {
-  return `.file-icon_type_${icon}{background-image:url("${
-    asserts.icons[icon]
-  }")}`
+  return `.file-icon_type_${icon}{background-image:url("${asserts.icons[icon]}")}`
 }
 
 function getCSS(files) {

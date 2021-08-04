3cacbf0e8c6b8c35c1bc5ca84ad7381f3ae3c81b
fisker Cheung <lionkay@gmail.com>
2021-08-04 16:42:47 +0800

    Tweak

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,14 @@ import replace from '@rollup/plugin-replace'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
+  output: [{
+    file: 'dist/index.cjs',
     format: 'cjs',
-  },
+exports: 'auto'
+  },{
+    file: 'dist/index.mjs',
+    format: 'esm',
+  },],
   plugins: [
     replace({
       './assets': '../.cache/assets',
